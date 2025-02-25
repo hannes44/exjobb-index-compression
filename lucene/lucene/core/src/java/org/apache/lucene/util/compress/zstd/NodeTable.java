63538fb6b25876984caabf8c72b83992e3107e6e
Martin Blom <89857077+Marre-B@users.noreply.github.com>
2025-02-25 14:14:23 +0100

    Reapply "ZSTD Compression Files (Java Errors)"
    This reverts commit 2c66bd49e15e74f8e53063bdbbd90c262db54581.

---
+++ lucene/lucene/core/src/java/org/apache/lucene/util/compress/zstd/NodeTable.java
@@ -0,0 +1,43 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
+package org.apache.lucene.util.compress.zstd;
+
+import java.util.Arrays;
+
+class NodeTable
+{
+    int[] count;
+    short[] parents;
+    int[] symbols;
+    byte[] numberOfBits;
+
+    public NodeTable(int size)
+    {
+        count = new int[size];
+        parents = new short[size];
+        symbols = new int[size];
+        numberOfBits = new byte[size];
+    }
+
+    public void reset()
+    {
+        Arrays.fill(count, 0);
+        Arrays.fill(parents, (short) 0);
+        Arrays.fill(symbols, 0);
+        Arrays.fill(numberOfBits, (byte) 0);
+    }
+
+    public void copyNode(int from, int to)
+    {
+        count[to] = count[from];
+        parents[to] = parents[from];
+        symbols[to] = symbols[from];
+        numberOfBits[to] = numberOfBits[from];
+    }
+}
